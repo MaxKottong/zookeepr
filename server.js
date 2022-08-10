1a743e0a03b91fffad949aedc5101a76c7de11b0
Max Kottong <max.kottong@gmail.com>
2022-08-10 12:58:24 -0400

    Added website assets

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -90,6 +91,22 @@ app.get('api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.post('/api/animals', (req, res) => {
